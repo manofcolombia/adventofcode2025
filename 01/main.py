bae37e70feadff320737d0acb919ee36c233292a
Zak Lantz <zlantz@liberty.edu>
2025-12-02 11:57:31 -0500

    reworked for day1 part 2

--- 01/main.py
+++ 01/main.py
@@ -1,9 +1,6 @@
 from pprint import pprint
 
-def main():
-    actual_combination = 0
-    dial_start = 50
-    dial_current = dial_start
+def import_rotations():
     dial_rotations = list()
 
     with open("input.txt", "r") as text_input:
@@ -18,19 +15,39 @@ def main():
         }
         dial_rotations.append(rotation)
 
+    return dial_rotations
+
+def exactly_zero_check(dial_current, dial_rotations):
+    actual_combination = 0
     for rotation in dial_rotations:
-        pprint(rotation)
         if rotation["direction"] == "L":
             dial_current = dial_current - rotation["amount"]
         else:
             dial_current = dial_current + rotation["amount"]
-        pprint(f"Current Dial: {dial_current}")
 
         # Check if the dial is at literal 0 or if divisable by 100 evenly
         if dial_current == 0 or dial_current % 100 == 0:
             actual_combination += 1
 
-    pprint(f"Actual Combination: {actual_combination}")
+    return actual_combination
+
+def passes_zero_check(dial_current, dial_rotations):
+    actual_combination = 0
+
+
+
+    return actual_combination
+
+def main():
+    dial_start = 50
+    dial_current = dial_start
+    dial_rotations = import_rotations()
+
+    num_exactly_zero = exactly_zero_check(dial_current, dial_rotations)
+    num_passes_zero = passes_zero_check(dial_current, dial_rotations)
+
+    pprint(f"Number of Exactly Zero Hits (Part 1): {num_exactly_zero}")
+    pprint(f"Number of Zero Passes (Part 2): {num_passes_zero}")
 
 if __name__ == "__main__":
     main()
